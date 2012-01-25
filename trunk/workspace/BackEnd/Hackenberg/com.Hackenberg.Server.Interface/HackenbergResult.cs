36469b436e7419b9b12a701ee28bcc31f840658b
sonesson.henrik@gmail.com <sonesson.henrik@gmail.com@b413f621-d0ea-d61f-103d-02baf72ec6c7>
2012-01-25 12:11:09 +0000

    Added HackenbergResult wrappers

---
+++ trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.Server.Interface/HackenbergResult.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace com.Hackenberg.Server.Interface
+{
+    [DataContract(Namespace = "http://code.google.com/p/hackenberg/namespace/20120125", IsReference = false)]
+    class HackenbergResult<T> : HackenbergResultBase
+    {
+        #region Properties
+
+        [DataMember(Order=10, EmitDefaultValue=false)]
+        public T Result { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public HackenbergResult()
+            : base()
+        {
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="status"></param>
+        /// <param name="message"></param>
+        public HackenbergResult(T result, HackenbergStatusCodes status = HackenbergStatusCodes.Ok, string message = null)
+            : this(status, message)
+        {
+            Result = result;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="status"></param>
+        /// <param name="message"></param>
+        public HackenbergResult(HackenbergStatusCodes status = HackenbergStatusCodes.Ok, string message = null)
+            : base(status, message)
+        {
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public HackenbergResult(HackenbergServiceException exception)
+            : base(exception)
+        {
+        }
+
+
+        /// <summary>
+        /// Throws an exception if result.status differs from "Ok"
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public static T ReturnResult(HackenbergResult<T> result)
+        {
+            if (result.Status != HackenbergStatusCodes.Ok)
+            {
+                throw new HackenbergServiceException(result.Status, result.Message);
+            }
+            return result.Result;
+        }
+    }
+}
---
+++ trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.Server.Interface/HackenbergResultBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace com.Hackenberg.Server.Interface
+{
+    [DataContract(Namespace = "http://code.google.com/p/hackenberg/namespace/20120125", IsReference = false, Name="HackenbergResult")]
+    public class HackenbergResultBase
+    {
+        #region Properties
+
+        [DataMember(Order = 0, IsRequired = true)]
+        public HackenbergStatusCodes Status { get; set; }
+
+        [DataMember(Order = 1, EmitDefaultValue = false)]
+        public string Message { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Sets Status to ok and message to null
+        /// </summary>
+        public HackenbergResultBase()
+        {
+            Status = HackenbergStatusCodes.Ok;
+            Message = null;
+        }
+
+
+        /// <summary>
+        /// Sets message to null if message == ""
+        /// </summary>
+        /// <param name="status"></param>
+        /// <param name="message"></param>
+        public HackenbergResultBase(HackenbergStatusCodes status = HackenbergStatusCodes.Ok, string message = null)
+        {
+            Status = status;
+            Message = message;
+            if (message == "")
+                Message = null;
+        }
+
+
+        /// <summary>
+        /// Exception instantiated result.
+        /// </summary>
+        /// <param name="exception"></param>
+        public HackenbergResultBase(HackenbergServiceException exception)
+            : this(exception.Reason, exception.Message)
+        {
+        }
+
+
+        /// <summary>
+        /// throws an exception if the result status differs from "Ok"
+        /// </summary>
+        /// <param name="result"></param>
+        public static void GetResult(HackenbergResultBase result)
+        {
+            if (result.Status != HackenbergStatusCodes.Ok)
+            {
+                throw new HackenbergServiceException(result.Status, result.Message);
+            }
+        }
+    }
+}
