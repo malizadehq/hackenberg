98451b0e4d89601722d48c6bd637f7acd334f254
sonesson.henrik@gmail.com <sonesson.henrik@gmail.com@b413f621-d0ea-d61f-103d-02baf72ec6c7>
2012-01-25 15:44:57 +0000

    Added data model logics to Sessions

--- trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Sessions/Session.cs
+++ trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Sessions/Session.cs
@@ -2,10 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
+using System.Collections.ObjectModel;
 
 namespace com.Hackenberg.DataModel.Sessions
 {
-    class Session
+    /// <summary>
+    /// 
+    /// </summary>
+    [CollectionDataContract(Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Sessions", IsReference = false)]
+    public class SessionList : ObservableCollection<Session>
     {
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract(Name = "session", Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Sessions")]
+    public class Session : NotifyPropertyChangedObject
+    {
+        #region Id(id) Property
+
+        private Guid? _id;
+        [DataMember(Name = "id", IsRequired = true, EmitDefaultValue = false, Order = 0)]
+        public Guid? Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+
+        #endregion
     }
 }
--- trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Sessions/SessionMember.cs
+++ trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Sessions/SessionMember.cs
@@ -2,10 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
+using System.Collections.ObjectModel;
 
 namespace com.Hackenberg.DataModel.Sessions
 {
-    class SessionMember
+    /// <summary>
+    /// 
+    /// </summary>
+    [CollectionDataContract(Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Sessions", IsReference = false)]
+    public class SessionMemberList : ObservableCollection<SessionMember>
     {
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract(Name = "sessionmember", Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Sessions")]
+    public class SessionMember : NotifyPropertyChangedObject
+    {
+        #region SessionId(sessionid) Property
+
+        private Guid? _sessionId;
+        [DataMember(Name = "sessionid", IsRequired = true, EmitDefaultValue = false, Order = 0)]
+        public Guid? SessionId
+        {
+            get
+            {
+                return _sessionId;
+            }
+            set
+            {
+                if (value != _sessionId)
+                {
+                    _sessionId = value;
+                    OnPropertyChanged("SessionId");
+                }
+            }
+        }
+
+        #endregion
+
+        #region UserId(userid) Property
+
+        private Guid? _userId;
+        [DataMember(Name = "userid", IsRequired = true, EmitDefaultValue = false, Order = 1)]
+        public Guid? UserId
+        {
+            get
+            {
+                return _userId;
+            }
+            set
+            {
+                if (value != _userId)
+                {
+                    _userId = value;
+                    OnPropertyChanged("UserId");
+                }
+            }
+        }
+
+        #endregion
     }
 }
