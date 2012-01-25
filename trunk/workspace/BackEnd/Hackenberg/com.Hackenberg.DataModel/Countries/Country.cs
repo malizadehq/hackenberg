f44e1a11b1f8f9ecc44ad60659e39c269912ee1c
sonesson.henrik@gmail.com <sonesson.henrik@gmail.com@b413f621-d0ea-d61f-103d-02baf72ec6c7>
2012-01-25 15:21:29 +0000

    Added datamodel logics for country classes

--- trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Countries/Country.cs
+++ trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Countries/Country.cs
@@ -2,10 +2,112 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
+using System.Collections.ObjectModel;
 
 namespace com.Hackenberg.DataModel.Countries
 {
-    class Country
+    /// <summary>
+    /// 
+    /// </summary>
+    [CollectionDataContract(Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Countries", IsReference = false)]
+    public class CountryList : ObservableCollection<Country>
     {
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract(Name = "country", Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Countries")]
+    public class Country : NotifyPropertyChangedObject
+    {
+        #region Id(id) Property
+
+        private Guid? _id;
+        [DataMember(Name = "id", IsRequired = true, EmitDefaultValue = false, Order = 0)]
+        public Guid? Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+
+        #endregion
+
+        #region ControlledBy(controlledby) Property
+
+        private Guid? _controlledBy;
+        [DataMember(Name = "controlledby", IsRequired = true, EmitDefaultValue = false, Order = 1)]
+        public Guid? ControlledBy
+        {
+            get
+            {
+                return _controlledBy;
+            }
+            set
+            {
+                if (value != _controlledBy)
+                {
+                    _controlledBy = value;
+                    OnPropertyChanged("ControlledBy");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Name(name) Property
+
+        private string _name;
+        [DataMember(Name = "name", IsRequired = true, Order = 2)]
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Points(points) Property
+
+        private int _points;
+        [DataMember(Name = "points", IsRequired = true, Order = 3)]
+        public int Points
+        {
+            get
+            {
+                return _points;
+            }
+            set
+            {
+                if (value != _points)
+                {
+                    _points = value;
+                    OnPropertyChanged("Points");
+                }
+            }
+        }
+
+        #endregion
     }
 }
