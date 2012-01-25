4fc45429f717e72f4095a90f6ee5cc57f380222c
sonesson.henrik@gmail.com <sonesson.henrik@gmail.com@b413f621-d0ea-d61f-103d-02baf72ec6c7>
2012-01-25 15:36:59 +0000

    Added datamodel logics for Maps

--- trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Maps/Map.cs
+++ trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Maps/Map.cs
@@ -2,10 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
+using System.Collections.ObjectModel;
 
 namespace com.Hackenberg.DataModel.Maps
 {
-    class Map
+    /// <summary>
+    /// 
+    /// </summary>
+    [CollectionDataContract(Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Maps", IsReference = false)]
+    public class MapList : ObservableCollection<Map>
     {
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract(Name = "map", Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Maps")]
+    public class Map : NotifyPropertyChangedObject
+    {
+        #region Id(id) Property
+
+        private Guid? _id;
+        [DataMember(Name = "id", IsRequired = true, EmitDefaultValue = false, Order = 0)]
+        public Guid? Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Name(name) Property
+
+        private string _name;
+        [DataMember(Name = "name", IsRequired = false, Order = 1)]
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        #endregion
     }
 }
