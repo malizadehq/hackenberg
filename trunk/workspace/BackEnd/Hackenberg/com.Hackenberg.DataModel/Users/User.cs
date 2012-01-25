363f2cd12a6b1dda3d4c25a19a17735f884023c7
sonesson.henrik@gmail.com <sonesson.henrik@gmail.com@b413f621-d0ea-d61f-103d-02baf72ec6c7>
2012-01-25 15:51:32 +0000

    Added data model logics to Users

--- trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Users/User.cs
+++ trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Users/User.cs
@@ -2,10 +2,90 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
+using System.Collections.ObjectModel;
 
 namespace com.Hackenberg.DataModel.Users
 {
-    class User
+    /// <summary>
+    /// 
+    /// </summary>
+    [CollectionDataContract(Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Users", IsReference = false)]
+    public class UserList : ObservableCollection<User>
     {
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract(Name = "user", Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Users")]
+    public class User : NotifyPropertyChangedObject
+    {
+        #region Id(id) Property
+
+        private Guid? _id;
+        [DataMember(Name = "id", IsRequired = true, EmitDefaultValue = false, Order = 0)]
+        public Guid? Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+
+        #endregion
+
+        #region UserName(username) Property
+
+        private string _userName;
+        [DataMember(Name = "username", IsRequired = true, Order = 1)]
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                if (value != _userName)
+                {
+                    _userName = value;
+                    OnPropertyChanged("UserName");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Password(password) Property
+
+        private string _password;
+        [DataMember(Name = "password", IsRequired = true, Order = 2)]
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (value != _password)
+                {
+                    _password = value;
+                    OnPropertyChanged("Password");
+                }
+            }
+        }
+
+        #endregion
     }
 }
