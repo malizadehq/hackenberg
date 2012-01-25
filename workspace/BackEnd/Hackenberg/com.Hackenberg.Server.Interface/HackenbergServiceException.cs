c9ce9997458e283cd6590212d649e63c5c805918
sonesson.henrik@gmail.com <sonesson.henrik@gmail.com@b413f621-d0ea-d61f-103d-02baf72ec6c7>
2012-01-25 10:13:21 +0000

    Added custom exceptions and error codes

---
+++ workspace/BackEnd/Hackenberg/com.Hackenberg.Server.Interface/HackenbergServiceException.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.Hackenberg.Server.Interface
+{
+    public class HackenbergServiceException : Exception
+    {
+        public HackenbergStatusCodes Reason { get; protected set; }
+
+        /// <summary>
+        /// Create a standard exception with an error code embedded.
+        /// </summary>
+        /// <param name="reason"></param>
+        public HackenbergServiceException(HackenbergStatusCodes reason)
+            : base(reason.ToDescriptionString())
+        {
+            Reason = reason;
+        }
+
+
+        /// <summary>
+        /// Create an exception with error code and message embedded.
+        /// </summary>
+        /// <param name="reason"></param>
+        /// <param name="message"></param>
+        public HackenbergServiceException(HackenbergStatusCodes reason, string message)
+            : base(string.IsNullOrEmpty(message) ? reason.ToDescriptionString() : message)
+        {
+            Reason = reason;
+        }
+
+
+        /// <summary>
+        /// Create an exception with error code, message and message args embedded.
+        /// </summary>
+        /// <param name="reason"></param>
+        /// <param name="message"></param>
+        /// <param name="args"></param>
+        public HackenbergServiceException(HackenbergStatusCodes reason, string message, params object[] args)
+            : base(string.Format(message, args))
+        {
+            Reason = reason;
+        }
+    }
+}
