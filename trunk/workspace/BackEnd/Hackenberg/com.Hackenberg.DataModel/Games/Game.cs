c591eb00a0f6bb13fdbd6971e6337b0b0c849d84
sonesson.henrik@gmail.com <sonesson.henrik@gmail.com@b413f621-d0ea-d61f-103d-02baf72ec6c7>
2012-01-25 15:34:21 +0000

    Added data model logics for Games

--- trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Games/Game.cs
+++ trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Games/Game.cs
@@ -2,10 +2,134 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
+using System.Collections.ObjectModel;
 
 namespace com.Hackenberg.DataModel.Games
 {
-    class Game
+    /// <summary>
+    /// 
+    /// </summary>
+    [CollectionDataContract(Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Games", IsReference = false)]
+    public class GameList : ObservableCollection<Game>
     {
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract(Name = "game", Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Games")]
+    public class Game : NotifyPropertyChangedObject
+    {
+        #region Id(id) Property
+
+        private Guid? _id;
+        [DataMember(Name = "id", IsRequired = true, EmitDefaultValue = false, Order = 0)]
+        public Guid? Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+
+        #endregion
+
+        #region SessionId(sessionid) Property
+
+        private Guid? _sessionId;
+        [DataMember(Name = "sessionid", IsRequired = true, EmitDefaultValue = false, Order = 1)]
+        public Guid? SessionId
+        {
+            get
+            {
+                return _sessionId;
+            }
+            set
+            {
+                if (value != _sessionId)
+                {
+                    _sessionId = value;
+                    OnPropertyChanged("SessionId");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Phase(phase) Property
+
+        private int _phase;
+        [DataMember(Name = "phase", IsRequired = false, Order = 2)]
+        public int Phase
+        {
+            get
+            {
+                return _phase;
+            }
+            set
+            {
+                if (value != _phase)
+                {
+                    _phase = value;
+                    OnPropertyChanged("Phase");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Turn(turn) Property
+
+        private int _turn;
+        [DataMember(Name = "turn", IsRequired = true, Order = 3)]
+        public int Turn
+        {
+            get
+            {
+                return _turn;
+            }
+            set
+            {
+                if (value != _turn)
+                {
+                    _turn = value;
+                    OnPropertyChanged("Turn");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Name(name) Property
+
+        private string _name;
+        [DataMember(Name = "name", IsRequired = false, Order = 4)]
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        #endregion
     }
 }
--- trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Games/GameRule.cs
+++ trunk/workspace/BackEnd/Hackenberg/com.Hackenberg.DataModel/Games/GameRule.cs
@@ -2,10 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
+using System.Collections.ObjectModel;
 
 namespace com.Hackenberg.DataModel.Games
 {
-    class GameRule
+    /// <summary>
+    /// 
+    /// </summary>
+    [CollectionDataContract(Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Games", IsReference = false)]
+    public class GameRuleList : ObservableCollection<GameRule>
     {
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract(Name = "gamerule", Namespace = "http://www.vfc.com/eCatalogue/20110307/com.Hackenberg.DataModel.Games")]
+    public class GameRule : NotifyPropertyChangedObject
+    {
+        #region RuleId(ruleid) Property
+
+        private Guid? _ruleId;
+        [DataMember(Name = "ruleid", IsRequired = true, EmitDefaultValue = false, Order = 0)]
+        public Guid? RuleId
+        {
+            get
+            {
+                return _ruleId;
+            }
+            set
+            {
+                if (value != _ruleId)
+                {
+                    _ruleId = value;
+                    OnPropertyChanged("RuleId");
+                }
+            }
+        }
+
+        #endregion
+
+        #region GameId(gameid) Property
+
+        private Guid? _gameId;
+        [DataMember(Name = "gameid", IsRequired = true, EmitDefaultValue = false, Order = 1)]
+        public Guid? GameId
+        {
+            get
+            {
+                return _gameId;
+            }
+            set
+            {
+                if (value != _gameId)
+                {
+                    _gameId = value;
+                    OnPropertyChanged("GameId");
+                }
+            }
+        }
+
+        #endregion
     }
 }
