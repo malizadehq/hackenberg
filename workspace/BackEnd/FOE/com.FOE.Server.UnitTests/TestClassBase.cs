cd6b808c7391da860648ffa5eebf70a3a37ecdc9
sonesson.henrik@gmail.com <sonesson.henrik@gmail.com@b413f621-d0ea-d61f-103d-02baf72ec6c7>
2012-01-27 11:08:50 +0000

    made AddUser actually add the user to the DB instead of just creating a mock. Renamed the laste (hopefully) hackenberg files to FOE

---
+++ workspace/BackEnd/FOE/com.FOE.Server.UnitTests/TestClassBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.FOE.Server.DataAccess.Database;
+using System.Web.Configuration;
+using System.Configuration;
+
+namespace com.FOE.Server.UnitTests
+{
+    [TestClass]
+    public class TestClassBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public TestClassBase()
+            : base()
+        {
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        protected static FOEDatabaseDataContext GetDatabaseConnection(string target)
+        {
+            Configuration webConfig = WebConfigurationManager.OpenWebConfiguration(string.Format("/{0}", target));
+            if (!webConfig.HasFile)
+            {
+                return null;
+            }
+            FOEDatabaseDataContext context = new FOEDatabaseDataContext(webConfig.ConnectionStrings.ConnectionStrings["FOESQLServer"].ConnectionString);
+            return context;
+        }
+    }
+}
