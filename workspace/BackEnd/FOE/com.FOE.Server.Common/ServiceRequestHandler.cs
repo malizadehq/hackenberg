c24453a694929f4e5c2563eaa2065d8da76dc22b
sonesson.henrik@gmail.com <sonesson.henrik@gmail.com@b413f621-d0ea-d61f-103d-02baf72ec6c7>
2012-02-14 21:46:16 +0000

    Changed AddUser to include Email. Added two primary keys to DB_Friends Fixed the Stats.TransportsLost stat

--- workspace/BackEnd/FOE/com.FOE.Server.Common/ServiceRequestHandler.cs
+++ workspace/BackEnd/FOE/com.FOE.Server.Common/ServiceRequestHandler.cs
@@ -44,8 +44,9 @@ namespace com.FOE.Server.Common
         /// </summary>
         /// <param name="userName"></param>
         /// <param name="password"></param>
+        /// <param name="email"></param>
         /// <returns></returns>
-        public User AddUser(string userName, string password)
+        public User AddUser(string userName, string password, string email)
         {
             DB_User da_user = DB_User.FromUser(new User() { Password = password, UserName = userName }, _context);
             if (da_user == null)
