6a96440250e0b22543f42bf5aa264169f0968104
iipigbear@hotmail.com <iipigbear@hotmail.com@b413f621-d0ea-d61f-103d-02baf72ec6c7>
2012-05-12 13:05:22 +0000

    Added support for 5 players, land is not tinted in the same color as the owners color. Borders are placed around all countries. Fps is horrible, might want to do something about that. The land-mask itself will reduce fps from 60 to 17. Create one colored version of each tile? Draw all tiles to new texture to avoid running 60*100 render-calls?

--- workspace/Editor/src/com/editor/assets.java
+++ workspace/Editor/src/com/editor/assets.java
@@ -29,9 +29,19 @@ public class assets
 	
 	// borders
 	public Texture		pIsoBorder_A;
+	public Texture		pIsoBorder_B;
+	public Texture		pIsoBorder_C;
+	public Texture		pIsoBorder_D;
+	public Texture		pIsoBorder_E;
+	
+	public Texture		pIsoLandMask;
 	
 	public Pixmap 		MapPixmap;
 	public Pixmap 		MapPixmap_Borders_A;
+	public Pixmap 		MapPixmap_Borders_B;
+	public Pixmap 		MapPixmap_Borders_C;
+	public Pixmap 		MapPixmap_Borders_D;
+	public Pixmap 		MapPixmap_Borders_E;
 	
 	public Texture		pIconsTexture;
 	public Texture		pButtonsTexture;
@@ -54,7 +64,14 @@ public class assets
 		pIsoGrassTexture	= new Texture(Gdx.files.internal("res/tiles/iso_grass.png"));
 		pGrassTexture		= new Texture(Gdx.files.internal("res/tiles/tile_grass_01.png"));
 		pEditorUiTexture	= new Texture(Gdx.files.internal("res/ui/editor_ui.png"));
+		
 		pIsoBorder_A		= new Texture(Gdx.files.internal("res/tiles/iso_borders_A.png"));
+		pIsoBorder_B		= new Texture(Gdx.files.internal("res/tiles/iso_borders_B.png"));
+		pIsoBorder_C		= new Texture(Gdx.files.internal("res/tiles/iso_borders_C.png"));
+		pIsoBorder_D		= new Texture(Gdx.files.internal("res/tiles/iso_borders_D.png"));
+		pIsoBorder_E		= new Texture(Gdx.files.internal("res/tiles/iso_borders_E.png"));
+		
+		pIsoLandMask		= new Texture(Gdx.files.internal("res/tiles/iso_land_mask.png"));
 		
 		pButtonsTexture		= new Texture(Gdx.files.internal("res/ui/buttons.png"));
 		pIconsTexture		= new Texture(Gdx.files.internal("res/ui/icons.png"));
@@ -63,8 +80,20 @@ public class assets
 		FileHandle fHand	= new FileHandle("res/maps/editor_map.png");
 		MapPixmap		 	= new Pixmap(fHand);
 	
-		FileHandle fHandB	= new FileHandle("res/maps/editor_map_borders_A.png");
-		MapPixmap_Borders_A = new Pixmap(fHandB);
+		FileHandle fHandBoA	= new FileHandle("res/maps/editor_map_borders_A.png");
+		MapPixmap_Borders_A = new Pixmap(fHandBoA);
+		
+		FileHandle fHandBoB	= new FileHandle("res/maps/editor_map_borders_B.png");
+		MapPixmap_Borders_B = new Pixmap(fHandBoB);
+		
+		FileHandle fHandBoC	= new FileHandle("res/maps/editor_map_borders_C.png");
+		MapPixmap_Borders_C = new Pixmap(fHandBoC);
+		
+		FileHandle fHandBoD	= new FileHandle("res/maps/editor_map_borders_D.png");
+		MapPixmap_Borders_D = new Pixmap(fHandBoD);
+		
+		FileHandle fHandBoE	= new FileHandle("res/maps/editor_map_borders_E.png");
+		MapPixmap_Borders_E = new Pixmap(fHandBoE);
 		
 		
 		pCreateSound 		= (Sound) Gdx.audio.newSound(Gdx.files.internal("res/sound/poff.wav"));
